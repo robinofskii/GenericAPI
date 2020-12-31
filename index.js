3976b03836cb4d849822cb39e5f3877ca305ef42
Robinofski <rob-vandenberg@outlook.com>
2020-12-31 02:51:04 +0100

    Started work on register route
    Seems to be a problem with the requests. req.body keeps being undefined and crashes the server.

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+const app = express()
+dotenv.config()
+
+// Routes import
+const authRoute = require('./routes/auth')
+
+// Routes Middlewares
+app.use('/auth', authRoute)
+
+// Middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello world!')
+})
+
+// Connect to Database
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log('Connected to database!')
+)
+
+// Start listening on port
+app.listen(
+  process.env.PORT,
+  () => console.log(`API running on port: ${process.env.PORT}`)
+)
