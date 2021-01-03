e40f20ddbc088fb7423dfa338dceab19023b49e0
Robinofski <rob-vandenberg@outlook.com>
2021-01-03 22:33:36 +0100

    Added route for random images

--- routes/images.js
+++ routes/images.js
@@ -2,14 +2,34 @@ const router = require('express').Router()
 const axios = require('axios').default
 
 router.get('/', async (req, res) => {
-  getPhotos()
+  getAllPhotos()
     .then(data => {
       res.send(data)
     })
 })
 
-function getPhotos () {
+router.get('/random', async (req, res) => {
+  getRandomPhoto()
+    .then(data => {
+      res.send(data)
+    })
+})
+
+function getAllPhotos () {
   return axios.get(`${process.env.UNSPLASH_API}/photos`, {
+    headers: { Authorization: `Client-ID ${process.env.UNSPLASH_ACCESS_KEY}` }
+  })
+    .then(res => {
+      return res.data
+    })
+    .catch(err => {
+      console.log(err)
+      return Promise.reject(err)
+    })
+}
+
+function getRandomPhoto () {
+  return axios.get(`${process.env.UNSPLASH_API}/photos/random`, {
     headers: { Authorization: `Client-ID ${process.env.UNSPLASH_ACCESS_KEY}` }
   })
     .then(res => {
